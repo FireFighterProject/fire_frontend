83c1aaaa796861e5f820c8d2b9e38f329b63d436
rjsgud49 <qkrrjsgud49@gmail.com>
2025-08-17 23:22:12 +0900

    tailwind add

--- fire-front/src/App.tsx
+++ fire-front/src/App.tsx
@@ -1,8 +1,10 @@
 
 const App = () => {
   return (
-    <div>
-      App Component
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Hello Tailwind v4 + React!
+      </h1>
     </div>
   );
 };
