0f121ee3df949105069c7f7789eae7727c16f50a
rjsgud49 <qkrrjsgud49@gmail.com>
2025-11-14 11:41:15 +0900

    활동페이지 디자인 일관화

--- fire-front/src/components/Activity/ActivityTable.tsx
+++ fire-front/src/components/Activity/ActivityTable.tsx
@@ -11,21 +11,21 @@ interface Props {
 const ActivityTable: React.FC<Props> = ({ vehicles, onReturn, onRelocate }) => {
     return (
         <div className="overflow-x-auto">
-            <table className="w-full border border-black text-sm text-center border-collapse">
+            <table className="w-full border  border-gray-300 text-sm text-center border-collapse">
                 <thead className="bg-gray-100">
                     <tr>
-                        <th className="border border-black px-3 py-2">시도</th>
-                        <th className="border border-black px-3 py-2">소방서</th>
-                        <th className="border border-black px-3 py-2">차종</th>
-                        <th className="border border-black px-3 py-2">호출명</th>
-                        <th className="border border-black px-3 py-2">용량</th>
-                        <th className="border border-black px-3 py-2">인원</th>
-                        <th className="border border-black px-3 py-2">AVL</th>
-                        <th className="border border-black px-3 py-2">PS-LTE</th>
-                        <th className="border border-black px-3 py-2">출동장소</th>
-                        <th className="border border-black px-3 py-2">내용</th>
-                        <th className="border border-black px-3 py-2">상태</th>
-                        <th className="border border-black px-3 py-2">조치</th>
+                        <th className="border border-gray-300 px-3 py-2">시도</th>
+                        <th className="border border-gray-300 px-3 py-2">소방서</th>
+                        <th className="border  border-gray-300 px-3 py-2">차종</th>
+                        <th className="border border-gray-300 px-3 py-2">호출명</th>
+                        <th className="border border-gray-300 px-3 py-2">용량</th>
+                        <th className="border border-gray-300 px-3 py-2">인원</th>
+                        <th className="border  border-gray-300 px-3 py-2">AVL</th>
+                        <th className="border  border-gray-300 px-3 py-2">PS-LTE</th>
+                        <th className="border  border-gray-300 px-3 py-2">출동장소</th>
+                        <th className="border  border-gray-300 px-3 py-2">내용</th>
+                        <th className="border  border-gray-300 px-3 py-2">상태</th>
+                        <th className="border  border-gray-300   px-3 py-2">조치</th>
                     </tr>
                 </thead>
                 <tbody>
